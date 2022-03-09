457afbafe7f7afa248b702b8e7f20ba212dd123f
fudeel <fadilmucia@gmail.com>
2022-03-09 12:52:52 +0100

    refactor

--- app.js
+++ app.js
@@ -8,19 +8,22 @@ const authRoutes = require("./routes/users");
 
 const BASE_URL = "/api/v1"
 
+console.log('BASE_HOST: ', process.env.BASE_HOST);
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    dbName: process.env.DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection Success.");
-  })
-  .catch((err) => {
-      console.table(process.env);
-    console.error("Mongo Connection Error", err);
-  });
+    .connect(process.env.MONGO_URI, {
+        dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connection Success.");
+    })
+    .catch((err) => {
+        console.log("MONGO_URI: ",process.env.MONGO_URI);
+        console.log("DB_NAME: ",process.env.DB_NAME);
+        console.error("Mongo Connection Error", err);
+    });
 
 const app = express();
 
@@ -28,14 +31,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/ping", (req, res) => {
-  return res.send({
-    error: false,
-    message: "Server is healthy",
-  });
+    return res.send({
+        error: false,
+        message: "Server is healthy",
+    });
 });
 
 app.use( BASE_URL + "/users", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server started listening on PORT : " + PORT);
+    console.log("Server started listening on PORT : " + PORT);
 });
