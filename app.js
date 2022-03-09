8cd02b2f995fdbe7eee91e32cdcfd8987a393885
fudeel <fadilmucia@gmail.com>
2022-03-09 13:26:50 +0100

    refactor

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const os = require("os");
-const hostname = os.hostname();
-const hostnameNetwork = os.networkInterfaces();
 
 
 require("dotenv").config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const authRoutes = require("./routes/users");
 
 const BASE_URL = "/api/v1"
 
 console.log('BASE_HOST: ', process.env.BASE_HOST);
-console.log('hostname: ', hostname);
-console.log('hostnameNetwork: ', hostnameNetwork);
 
 mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGODB_URI, {
         dbName: process.env.DB_NAME,
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        family: 4
     })
     .then(() => {
         console.log("Database connection Success.");
     })
     .catch((err) => {
-        console.log("MONGO_URI: ",process.env.MONGO_URI);
+        console.log("MONGODB_URI: ",process.env.MONGO_URI);
         console.log("DB_NAME: ",process.env.DB_NAME);
         console.error("Mongo Connection Error", err);
     });
